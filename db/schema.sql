87c03ca311d121a18aef0657c0f446178896ebb7
Marco Mata <patrickstar1281@yahoo.com>
2022-07-20 06:25:37 -0500

    set up the voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,14 @@
 -- this will drop/delete the tables everytime we run schema.sql
 DROP TABLE IF EXISTS candidates;
+
 DROP TABLE IF EXISTS parties;
+
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
+
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -13,4 +16,12 @@ CREATE TABLE candidates (
     party_id INTEGER,
     industry_connected BOOLEAN NOT NULL,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
